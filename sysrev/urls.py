8e31699c7bd08a3e0a4fb9082660c693e221c7d3
stesesia <sesia.stefano@gmail.xom>
2016-03-19 17:59:20 +0000

    Added authentications, and edited database, please modify GUI

--- sysrev/urls.py
+++ sysrev/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^dashboard/', views.dashboard),
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]

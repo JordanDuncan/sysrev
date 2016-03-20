e1c583e2b744f446f9ba0a5aa004453ef341d74e
Jordan Duncan <jordan@dogfi.sh>
2016-03-20 23:45:44 +0000

    storing while merging stefanos changes with master

--- sysrev/views.py
+++ sysrev/views.py
@@ -34,6 +34,9 @@ def register(request):
             profile = profile_form.save(commit=False)
             profile.user = user
 
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
             profile.save()
 
             registered = True
